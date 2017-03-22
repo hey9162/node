83f035beb125b44e4fdb6340acce527ce336e3be
hueryong <1070931076@qq.com>
2017-03-22 19:11:12 +0800

    完成

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ var express = require('express')
 var app = express()
 app.use(express.static('wwwroot'))
 
-app.get('/',function(req,res){
-    
+app.get('/user',function(req,res){
+    console.log(req.query);
+    var name = req.query.name;
+    var phone = req.query.phone;
+    res.status(200).send('<strong>你提交的数据是：</strong><br>'+ '<h1>'+ name +'</h1>'+'<h1>'+ phone +'</h1>')
+})
+app.listen(3000, function () {
+    console.log('Node Server OK')
 })
